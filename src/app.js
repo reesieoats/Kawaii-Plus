a1de7d4c149bea0a7552b0e16c31ac75dfcfea21
reesieoats <reesieezy@gmail.com>
2020-08-04 19:10:56 -0400

    added iconElement to JS

--- src/app.js
+++ src/app.js
@@ -46,8 +46,9 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/10d@2x.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "595adbf6a3ae27cee86c95730ea72e7a";
