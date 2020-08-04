f09ca4bfd6cecd55599a91ea0811313394227ece
reesieoats <reesieezy@gmail.com>
2020-08-03 22:25:58 -0400

    added wind speed element to JS

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let speedElement = document.querySelector("#speed");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
+  speedElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "595adbf6a3ae27cee86c95730ea72e7a";
