0bbe4088f925989dc07a74055dea8006379fa578
reesieoats <reesieezy@gmail.com>
2020-08-03 22:01:23 -0400

    added function to display temp on JS

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data);
+}
+
 let apiKey = "595adbf6a3ae27cee86c95730ea72e7a";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
+axios.get(url).then(displayTemperature);
