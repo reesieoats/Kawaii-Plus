127937b8bd0fbf9b7a61289a13f0348993d8b568
reesieoats <reesieezy@gmail.com>
2020-08-03 22:18:39 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  let description = document.querySelector("#description");
+  let descriptionElement = document.querySelector("#description");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
