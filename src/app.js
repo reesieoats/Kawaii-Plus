8a7f9bf805745da65f3996941644cc595dd3b4b5
reesieoats <reesieezy@gmail.com>
2020-08-05 17:50:08 -0400

    removed alt function from JS

--- src/app.js
+++ src/app.js
@@ -47,7 +47,6 @@ function displayTemperature(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function search(city) {
