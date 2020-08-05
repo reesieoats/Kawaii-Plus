9d03418e5b1c0a76bc693ad2049116f88fbe54c5
reesieoats <reesieezy@gmail.com>
2020-08-05 17:55:15 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -56,9 +56,6 @@ function search(city) {
   let apiKey = "595adbf6a3ae27cee86c95730ea72e7a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
-
-  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(dispalyForecast);
 }
 
 function handleSubmit(event) {
